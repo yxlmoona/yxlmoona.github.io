05022dcd6d758d2117297e49c470b51c050fa768
Matthew Kangvanvongsa <matthewmetaveemomo@Matthews-MacBook-Pro.local>
2020-02-08 11:47:21 -0600

    start of carousel

--- cocktail/js/app.js
+++ cocktail/js/app.js
@@ -8,12 +8,12 @@ $(() => {
     }).then(
         (data)=>{
             data.drinks.forEach((i) => {
-              const $div = $('<div>').addClass('search-imgs')
-              $div.appendTo($('.cocktail-imgs'))
+              // const $div = $('<div>').addClass('search-imgs')
+              // $div.appendTo($('.cocktail-imgs'))
               const $img = $('<img>').attr('src',i.strDrinkThumb)
-              $img.appendTo($div)
-              const $h3 = $('<h3>').text(i.strDrink)
-              $h3.appendTo($div)
+              $img.appendTo($('.cocktail-imgs'))
+              // const $h3 = $('<h3>').text(i.strDrink)
+              // $h3.appendTo($div)
             })//close forEach
             // console.log(data);
 
