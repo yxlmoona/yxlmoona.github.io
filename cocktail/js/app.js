be67f1a7e326425c3e79451080f49ae0255a9508
Matthew Kangvanvongsa <matthewmetaveemomo@Matthews-MacBook-Pro.local>
2020-02-08 16:51:17 -0600

    random logic complete

--- cocktail/js/app.js
+++ cocktail/js/app.js
@@ -1,19 +1,36 @@
 
 //onload function
 $(() => {
-  $('#random').on('click', () => {
+  $('#random').on('click', (event) => {
+    $('h6').empty()
+    $('.cocktail-imgs').empty()
     $.ajax({
       url:'https://www.thecocktaildb.com/api/json/v1/1/random.php'
     }).then(
       (data) => {
-        $('.cocktail-imgs').empty()
+        const EventHandlers = {
+
+          showInstruction: () => {
+            const $h1 = $('<h6>').text(data.drinks[0].strIngredient1)
+            const $h2 = $('<h6>').text(data.drinks[0].strIngredient2)
+            const $h3 = $('<h6>').text(data.drinks[0].strIngredient3)
+            const $h4 = $('<h6>').text(data.drinks[0].strIngredient4)
+            const $h5 = $('<h6>').text(data.drinks[0].strIngredient5)
+            const $h6 = $('<h6>').text(data.drinks[0].strInstructions)
+            $('.main-ingredient').append($h1,$h2,$h3,$h4,$h5)
+            $('.instruction').append($h6)
+            // console.log(data.drinks[0].strInstructions)
+          }
+        }
+        console.log(data);
         const $div = $('<div>').addClass('search-imgs')
         $div.appendTo($('.cocktail-imgs'))
         const $img = $('<img>').attr('src',data.drinks[0].strDrinkThumb)
         $img.appendTo($div)
         const $h3 = $('<h3>').text(data.drinks[0].strDrink)
         $h3.appendTo($div)
-        
+
+        $('.cocktail-imgs img').on('click', EventHandlers.showInstruction)
       },
       () => {
         console.log('bad input');
@@ -22,6 +39,7 @@ $(() => {
   })//close click random
   $('#search').on('click', () => {
     $('.cocktail-imgs').empty()
+    $('h6').empty()
     const userInput = $('input').val()
     $.ajax({
         url:`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${userInput}`
